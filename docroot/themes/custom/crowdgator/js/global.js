e8762253d1c1edfe03fdfac7a1f7c636d579135a
irving-frias <irvingfrias41@gmail.com>
2023-05-31 08:33:03 -0700

    CG-000: Added changes to the site.

--- docroot/themes/custom/crowdgator/js/global.js
+++ docroot/themes/custom/crowdgator/js/global.js
@@ -3,14 +3,30 @@
  * Global utilities.
  *
  */
-(function ($, Drupal) {
+(function ($, Drupal, drupalSettings) {
 
   'use strict';
+  var api_key;
 
   Drupal.behaviors.crowdgator = {
     attach: function (context, settings) {
+      api_key = drupalSettings.open_ai.api_key ?? '';
+      console.log(api_key);
 
+      fetch('https://api.openai.com/v1/images/generations', {
+          method: 'POST',
+          headers: {
+              'Content-Type': 'application/json',
+              'Authorization': 'Bearer ' + api_key
+          },
+          // body: '{\n    "prompt": "a white siamese cat",\n    "n": 1,\n    "size": "1024x1024"\n  }',
+          body: JSON.stringify({
+              'prompt': 'a white siamese cat',
+              'n': 1,
+              'size': '1024x1024'
+          })
+      });
     }
   };
 
-})(jQuery, Drupal);
+})(jQuery, Drupal, drupalSettings);
